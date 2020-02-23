0ec3327e2d9698770c6caed1d2d2c705f43b19d6
JonasMartins <jonas.w.martins@gmail.com>
2020-02-22 23:42:01 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### Project based on the following tutorial:
+
+[Original Source](https://www.youtube.com/watch?v=_DqPiZPKkgY&list=PLMhAeHCz8S3_pgb-j51QnCEhXNj5oyl8n)
---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	MONGODB:
+		'mongodb+srv://Admin:o9Uz97jKpbjA07gH@js-fkcfi.mongodb.net/test?retryWrites=true&w=majority',
+	SECRET_KEY: 'some very secret key'
+};
